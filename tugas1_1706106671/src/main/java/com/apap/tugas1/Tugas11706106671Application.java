34c911f6486ad643deab5c09e25a6dee5df99f9b
David Elkana <davidelkana@Davids-MacBook-Pro.local>
2018-04-02 21:02:24 +0700

    Initial Project

---
+++ tugas1_1706106671/src/main/java/com/apap/tugas1/Tugas11706106671Application.java
@@ -0,0 +1,12 @@
+package com.apap.tugas1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tugas11706106671Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tugas11706106671Application.class, args);
+	}
+}
